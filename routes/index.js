aca232e379634e2a8bef0f7b2eb6b99c19422c71
Your Name <you@example.com>
2022-03-24 09:51:59 +0530

    1.0

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ router.post('/gen', (req, res) => {
     console.log('App successfully created. ', 'Packing app');
     Zip.zip(path, `${data.name}.zip`).then(() => {
       console.log('packed successfully. ', 'sending file');
+      res.setHeader('Access-Control-Allow-Origin','https://web2linux.web.app')
       res.download(`${data.name}.zip`)
 
       setTimeout(() => {
