7a20066fa8a1f690aba3bff5f06fd58af6d5869e
Your Name <you@example.com>
2022-03-24 11:19:22 +0530

    1.2

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ let fs = require('fs')
 const { exec } = require('child_process');
 var Zip = require('zip-a-folder');
 
+let nativefier = require('nativefier')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -23,8 +24,9 @@ router.post('/gen', (req, res) => {
   if (data.logo == null) {
     data.logo = 'https://logopond.com/logos/764befce2161b53b5895108e1e8597d7.png'
   }
-  exec('nativefier ' + data.site, (e, r) => {
+  exec('nativefier '+data.site,(e,r)=>{
     let path = r.split('App built to ')[1].split(', move to wherever it')[0]
+    console.log(e,path);
     console.log('App successfully created. ', 'Packing app');
     Zip.zip(path, `${data.name}.zip`).then(() => {
       console.log('packed successfully. ', 'sending file');
@@ -34,10 +36,12 @@ router.post('/gen', (req, res) => {
         console.log('Deleting files')
         fs.unlinkSync(`${data.name}.zip`)
         fs.rmSync(path, { recursive: true, force: true });
-      }, 1000 * 60 * 3)
+      }, 1000 * 60 * 5)
 
     })
+
   })
+
 })
 
 module.exports = router;
