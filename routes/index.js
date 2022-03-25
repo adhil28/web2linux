c155bd260fa48193d8a8d847139d38b8851fd85c
Your Name <you@example.com>
2022-03-25 15:36:08 +0530

    1.5

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,24 @@ let fs = require('fs')
 const { exec } = require('child_process');
 var Zip = require('zip-a-folder');
 
-
-/* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 router.post('/gen', (req, res) => {
-
-
   let data = req.body
 
-  console.log(data);
   data.name = data.name + Math.floor((Math.random() * 1000000) + 1);
   data.site = data.site.replace('https://', '')
-
   res.setHeader('Access-Control-Allow-Origin', '*')
 
-  if (data.logo == null) {
-    data.logo = 'https://logopond.com/logos/764befce2161b53b5895108e1e8597d7.png'
-  }
-  exec(`nativefier ${data.site}`  , (e, r) => {
+  createLinuxApp(data, res)
+})
+
+
+function createLinuxApp(data, res) {
+  exec(`nativefier ${data.site}`, (e, r) => {
     let path = r.split('App built to ')[1].split(', move to wherever it')[0]
     console.log(e, r);
     console.log('App successfully created. ', 'Packing app');
@@ -35,12 +33,11 @@ router.post('/gen', (req, res) => {
         console.log('Deleting files')
         fs.unlinkSync(`${data.name}.zip`)
         fs.rmSync(path, { recursive: true, force: true });
-      }, 1000 * 60 * 5)
+      }, 1000 * 60 * 2)
 
     })
 
   })
-
-})
+}
 
 module.exports = router;
